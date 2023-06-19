4f662bc083dd57a2431dc96fb31a825e3e528760
julio7173 <juliocesarsevericheorellana@gmail.com>
2023-06-19 05:19:55 -0400

    Iniciando de nuevo

--- src/pp/ManuApp.java
+++ src/pp/ManuApp.java
@@ -1,10 +1,7 @@
 package pp;
 
 import java.awt.BorderLayout;
-import java.awt.Image;
-import javax.swing.Icon;
 import javax.swing.ImageIcon;
-import javax.swing.JLabel;
 import javax.swing.JPanel;
 
 public class ManuApp extends javax.swing.JFrame {
